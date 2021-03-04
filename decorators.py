7809cddac9f638de14245b0de001e5ca29c3db95
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-04 20:10:02 +0530

    hi_p = decorater_function()

--- decorators.py
+++ decorators.py
@@ -1,10 +1,12 @@
 
-def decorater_function():
-    message = 'Hi'  # free variable
+def decorater_function(msg):
+    message = msg  # free variable
 
     def wrapper_function():
         print(message)
     return wrapper_function
 
-p = decorater_function()
-p()
+hi_p = decorater_function("hi")
+bye_p = decorater_function("bye")
+hi_p()
+bye_p()
