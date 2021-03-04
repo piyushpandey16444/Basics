ae49336adcd6ad567389c23921976c773678c8fb
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-04 20:29:21 +0530

    original_function

--- decorators.py
+++ decorators.py
@@ -1,6 +1,12 @@
 
 def decorater_function(original_function):
+    """
+    This is the decorater function that takes original function
+    """
     def wrapper_function():
+        """
+        this is wrapper function that when executed executes original function.
+        """
         print(f"THIS IS DECORATOR FUNCT !")
         return original_function()
     return wrapper_function
