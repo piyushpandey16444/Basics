f099121d1a40b69d6f3be60bedd4656a70661647
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-18 12:12:19 +0530

    views updated for handling form submit

--- AJAXify-Django-Forms-master/src/yourapp/views.py
+++ AJAXify-Django-Forms-master/src/yourapp/views.py
@@ -1,6 +1,7 @@
 # yourapp.views.py
 
 from django.views.generic import FormView
+from django.http import JsonResponse
 
 from .forms import JoinForm
 
@@ -9,3 +10,21 @@ class JoinFormView(FormView):
     form_class = JoinForm
     template_name = 'forms/ajax.html'
     success_url = '/form-success/'
+
+    def form_invalid(self, form):
+        response = super(JoinFormView, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super(JoinFormView, self).form_valid(form)
+        if self.request.is_ajax():
+            print(form.cleaned_data)
+            data = {
+                'message': "Successfully submitted form data."
+            }
+            return JsonResponse(data)
+        else:
+            return response
